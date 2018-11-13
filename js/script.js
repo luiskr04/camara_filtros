02d412eaf7c27587ce7dd2f54bf2fb4057e31fe3
luiskr04 <luis.angel.dh@gmail.com>
2018-11-13 00:41:34 -0600

    primer actualizacion de archivos

---
+++ js/script.js
@@ -0,0 +1,84 @@
+/*Se crea un array del tipo JSON*/
+var app = {
+	inicio: function(){							//Se define el primer elemento del objeto	
+		this.iniciarFastClick();				//Se llama a dos funciones principales
+		this.iniciaBoton();
+	},
+
+	iniciarFastClick: function(){				//se define el segundo elemnto del objeto
+		FastClick.attach(document.body);		//Se agrega la loibreria FastClick al cuerpo de la pagina
+	},
+
+	iniciaBoton: function(){					//Se define el tercer elemento del objeto
+		var buttonAction = document.querySelector("#boton-act");		//se crea una variable a partir del elemento que corresponde el identificador 
+		buttonAction.addEventListener('click', function(){
+			app.cargarFoto(Camera.PictureSourceType.CAMERA);
+		});			//Se agrega un evento al elemnto del tipo Listener para identificar cuando sea clickeado
+		
+		//Filtros
+		var filterButtons = document.querySelectorAll('.boton-filtro');
+		filterButtons[0].addEventListener('click', function(){
+			app.aplicarFiltro('gray');
+		});
+		filterButtons[1].addEventListener('click', function(){
+			app.aplicarFiltro('negative');
+		});
+		filterButtons[2].addEventListener('click', function(){
+			app.aplicarFiltro('sepia');
+		});
+	},
+
+	/*Acceso al plugin de cordova*/
+	cargarFoto: function(pictureSourceType){
+		var opciones = {
+			quality:50,				//calidad de la foto
+			sourceType:pictureSourceType,
+			destinationType: Camera.DestinationType.FILE_URI,		//destino de la foto
+			targetWidth:300,			//ancho de la foto
+			targetHeight:300,			//alto de la foto
+			correctOrientation: true
+		};
+
+		navigator.camera.getPicture(app.fotoTomada, app.errorAlTomarFoto, opciones);
+	},
+
+	fotoTomada: function(imageURI){
+		var image = document.createElement('img');
+
+		img.onload = function(){
+			app.pintarFoto(img);
+		}
+		img.src = imageURI;
+	},
+
+	//funcion pintar foto
+	pintarFoto: function(img){
+		var canvas = document.querySelector('#foto');
+		var context = canvas.getContext('2d');
+		canvas.width = img.width;
+		canvas.height = img.height;
+		context.drawImage(img, 0, 0, img.width, img.height);
+	},
+
+	errorAlTomarFoto: function(){
+		console.log('Fallo al tomar foto o foto cancelada: '+message);
+	},
+
+	aplicarFiltro: function(filterName){
+		var canvas = document.querySelector('#foto');
+		var context = canvas.getContext('2d');
+
+		imageData = context.getImageData(0,0,canvas.width,canvas.height);
+
+		effects[filterName] (imageData.data);
+		context.putImageData(imageData, 0, 0);
+	}
+};			//Fin del objeto JSON
+
+var imageData;
+if('addEventListener' in document){
+	document.addEventListener('DOMContentLoaded', function(){
+		app.inicio();
+	},false);
+
+}
